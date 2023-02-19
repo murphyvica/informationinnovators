4be518b675dcefffa3102345b2090d578daadc7d
murphyvica <murphyv@uw.edu>
2023-02-19 00:16:38 -0800

    added cleaned test data and script

---
+++ DataCleaning/test_data_clean.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from pathlib import Path
+
+prod = pd.read_csv("data/products_price.csv")
+
+# Cleaning null price values
+
+prod.price[prod.price < 0] = np.nan
+prod = prod.dropna(subset=["price"])
+
+# Categorizing color
+
+prod.color[prod.color.isna()] = "None"
+prod.color[prod.color == "_"] = "None"
+
+colorCat = ["red", "orange", "yellow", "green", "blue", "purple", "violet", "gold", "silver", \
+            "pink", "cyan", "brown", "black", "magenta", "white", "transparent", "navy", "olive", \
+            "cream", "wine", "maroon", "beige", "gray", "multicolor"]
+
+prod.color = prod.color.str.lower()
+
+colorGroup = []
+for col in prod.color:
+    string = str(col)
+    chosen = process.extractOne(string, colorCat, scorer=fuzz.partial_ratio)
+    colorGroup.append(chosen[0])
+
+prod['color'] = pd.Series(colorGroup)
+prod.color[prod.color.isna()] = "Other"
+
+# Dropping Size column
+
+prod = prod.drop("size", axis=1)
+
+# Creating 2D array of time and price
+
+np.set_printoptions(suppress=True)
+
+timePrice = prod[["time", "price"]]
+A = timePrice.to_numpy()
+
+prod['timePrice'] = A.tolist()
+
+prodShort = prod.groupby(['asin', 'product_type', 'parent_asin', 'product_group', 'manufacturer', 'brand', \
+                          'model', 'color', 'is_eligible_for_super_saver_shipping', 'is_sns'], group_keys=True, \
+                         sort=False).timePrice.apply(np.array).reset_index()
+
+# Output to csv
+
+filepath = Path('CleanedData/testdata.csv')
+filepath.parent.mkdir(parents=True, exist_ok=True)
+prodShort.to_csv(filepath)
+
+
+
+
+
